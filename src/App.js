de35ae6346261c1f6e6a1435bceb63226f9306ad
Chris Strnad <strnadchristopher@gmail.com>
2022-03-19 06:44:11 -0500

    init
    react chat app made in 1 hour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState, useEffect} from 'react';
 function App() {
+  const [allChats, setAllChats] = useState([{text:"Hi it's me greg.", user:"Greg"}, {text:"What's up", user:"Bartholemew"}]);
+  const addChat = (newChat) =>{
+    setAllChats([...allChats, newChat])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatHistory setAllChats={setAllChats} allChats={allChats}/>
+      <ChatInput addChat={addChat}/>
     </div>
   );
 }
+function ChatHistory(props){
+  let {allChats, setAllChats} = props;
+  let chatsObjects = allChats.map((chatItem)=>{
+    return <ChatMessage text={chatItem.text} user={chatItem.user}/>
+  })
+  useEffect(() =>{
+    setAllChats(allChats);
+    // TODO: this breaks everything, find out why
+    //chatsObjects[chatsObjects.length - 1].scrollIntoView( {behavior:"smooth"} );
+  }, [allChats, setAllChats, chatsObjects])
+  return (
+    <div className="chatHistory">
+      {chatsObjects}
+    </div>
+  )
+}
 
+function ChatMessage(props){
+  const [messageClass, setMessageClass] = useState("");
+  return (
+    <div className={`chatMessage ${props.user == "Me" ? 'userMessage' : ''}`}>
+      <span className='chatText'>{props.user}: {props.text}</span>  
+    </div>
+  )
+}
+
+function ChatInput(props){
+  const [messageText, setMessageText] = useState("");
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    props.addChat({text:messageText, user:"Me"})
+    setMessageText("")
+  }
+  const handleInput = (e) => {
+    setMessageText(e.target.value);
+  }
+  return (
+    <div className="chatInput">
+      <form onSubmit={handleSubmit} className='chatForm'>
+        <input onChange={handleInput} value={messageText} className='chatBox'>
+        </input>
+        <button type='submit' value='Submit' className="chatSubmit">
+          Send
+        </button>
+      </form>
+    </div>
+  )
+}
 export default App;
